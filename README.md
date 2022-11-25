591a1edfa9cda961ebd93dba24ee71baf70480ac
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-11-25 11:15:57 -0500

    kubeshark:37.0

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.kubeshark
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-kubeshark/actions/workflows/build.yml/badge.svg)
+
+Installs the [kubeshark](https://kubeshark.co/) cli.
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.kubeshark
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+set -e
+
+DIR=~/Downloads
+MIRROR=https://github.com/kubeshark/kubeshark/releases/download
+
+dl()
+{
+    local app="kubeshark"
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="${os}_${arch}"
+    # https://github.com/kubeshark/kubeshark/releases/download/37.0/kubeshark_darwin_amd64.sha256
+    local url=$MIRROR/$ver/${app}_${platform}.sha256
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSLf $url | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
+    dl $ver windows amd64
+}
+
+dl_ver ${1:-37.0}
