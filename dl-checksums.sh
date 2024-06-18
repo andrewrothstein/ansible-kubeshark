47d92b409830ad0895c5a903b5c11cbc676f095e
Andrew Rothstein <andrew.rothstein@gmail.com>
2024-06-18 01:34:13 +0000

    kubeshark:52.3.68

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -1,46 +1,27 @@
 #!/usr/bin/env sh
 set -e
-
-DIR=~/Downloads
 MIRROR=https://github.com/kubeshark/kubeshark/releases/download
 
 dl()
 {
     local ver=$1
-    local lchecksums=$2
-    local os=$3
-    local arch=$4
+    local os=$2
+    local arch=$3
     local platform="${os}_${arch}"
-    local file="kubeshark_${ver}_${platform}.tar.gz"
-    # https://github.com/kubeshark/kubeshark/releases/download/v51.0.39/kubeshark_51.0.39_darwin_amd64.tar.gz
-    local url="$MIRROR/v$ver/${file}"
+    # https://github.com/kubeshark/kubeshark/releases/download/v52.3.68/kubeshark_darwin_amd64.sha256
+    local url="$MIRROR/v$ver/kubeshark_${platform}.sha256"
     printf "    # %s\n" $url
-    printf "    %s: sha256:%s\n" $platform $(grep -e "${file}" $lchecksums | awk '{ print $1 }')
+    printf "    %s: sha256:%s\n" $platform $(curl -SsLf $url | awk '{ print $1; }')
 }
 
-dl_ver() {
+dl_ver()
+{
     local ver=$1
-
-    # https://github.com/kubeshark/kubeshark/releases/download/v51.0.39/kubeshark_51.0.39_checksums.txt
-    local lfile="kubeshark_${ver}_checksums.txt"
-    local lchecksums="${DIR}/${lfile}"
-    local checksumsurl="${MIRROR}/v${ver}/${lfile}"
-
-    if [ ! -e $lchecksums ];
-    then
-        curl -sSLf -o $lchecksums $checksumsurl
-    fi
-
-    printf "  # %s\n" $checksumsurl
     printf "  '%s':\n" $ver
-    dl $ver $lchecksums darwin amd64
-    dl $ver $lchecksums darwin arm64
-    dl $ver $lchecksums linux 386
-    dl $ver $lchecksums linux amd64
-    dl $ver $lchecksums linux arm64
-    dl $ver $lchecksums windows 386
-    dl $ver $lchecksums windows amd64
-    dl $ver $lchecksums windows arm64
+    dl $ver darwin amd64
+    dl $ver darwin arm64
+    dl $ver linux amd64
+    dl $ver linux arm64
 }
 
-dl_ver ${1:-52.1.45}
+dl_ver ${1:-52.3.68}
